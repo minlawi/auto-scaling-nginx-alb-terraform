e6712cae69dde3f201b5598f4c5c83c03365883e
minlawi <minlawi1992@gmail.com>
2025-03-07 17:51:49 +0800

    Updated output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "alb_dns_name" {
-  value = var.create_vpc ? aws_alb.app_lb[0].dns_name : null
-}
+# output "alb_dns_name" {
+#   value = var.create_vpc ? aws_alb.app_lb[0].dns_name : null
+# }
 
-output "bastion_public_ip" {
-  value = var.create_bastion ? aws_instance.bastion_instance[0].public_ip : null
-}
+# output "bastion_public_ip" {
+#   value = var.create_bastion ? aws_instance.bastion_instance[0].public_ip : null
+# }
