33fb03924035be4887a0ff5357c5851437737b22
minlawi <minlawi1992@gmail.com>
2025-03-07 17:07:34 +0800

    Added providers.tf

---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.89.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  profile = var.profile
+}
+
+locals {
+  anywhere        = "0.0.0.0/0"
+  http_port       = 80
+  http_protocol   = "HTTP"
+  tcp_protocol    = "tcp"
+  all             = -1
+  ssh             = 22
+  server_t2_micro = "t2.micro"
+  icmp_protocol   = "icmp"
+}
