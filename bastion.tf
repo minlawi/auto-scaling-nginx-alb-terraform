5671d14670e601239f6932f5a9d6b5e9ec5ad9a0
minlawi <minlawi1992@gmail.com>
2025-03-07 18:07:37 +0800

    Updated bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -19,22 +19,22 @@
 #   }
 # }
 
-# resource "aws_instance" "bastion_instance" {
-#   count                       = var.create_bastion ? 1 : 0
-#   ami                         = data.aws_ami.ubuntu.id
-#   instance_type               = local.server_t2_micro
-#   subnet_id                   = aws_subnet.public_subnet[0].id
-#   availability_zone           = data.aws_availability_zones.available.names[0]
-#   key_name                    = var.create_bastion ? aws_key_pair.public_key[0].key_name : null
-#   associate_public_ip_address = true
-#   security_groups             = [aws_security_group.bastion_sg[0].id]
-#   user_data_replace_on_change = true
-#   tags = {
-#     Name = "Bastion-Jumphost"
-#   }
-#   lifecycle {
-#     ignore_changes = [
-#       security_groups
-#     ]
-#   }
-# }
+resource "aws_instance" "bastion_instance" {
+  count                       = var.create_bastion ? 1 : 0
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = local.server_t2_micro
+  subnet_id                   = aws_subnet.public_subnet[0].id
+  availability_zone           = data.aws_availability_zones.available.names[0]
+  key_name                    = var.create_bastion ? aws_key_pair.public_key[0].key_name : null
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.bastion_sg[0].id]
+  user_data_replace_on_change = true
+  tags = {
+    Name = "Bastion-Jumphost"
+  }
+  lifecycle {
+    ignore_changes = [
+      security_groups
+    ]
+  }
+}
