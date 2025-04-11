ff2c031bceedf67da6c55af061c2150bdab3526d
minlawi <minlawi1992@gmail.com>
2025-04-12 01:20:39 +0800

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -25,7 +25,6 @@ variable "create_bastion" {
 variable "active_environment" {
   description = "Choose the active environment for the load balancer. Options are 'blue' or 'green'."
   type        = string
-  # default     = ""
   validation {
     condition     = contains(["blue", "green"], var.active_environment)
     error_message = "The active_environment variable must be either 'blue' or 'green'."
