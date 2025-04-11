c0a79c66560b7ff6cb9cb6a304120d66ce3e5e2b
minlawi <minlawi1992@gmail.com>
2025-04-11 22:38:25 +0800

    Updated userdata folder

--- userdata/blue.sh
+++ userdata/blue.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-
-# Update and install Apache
 sudo apt update -y
 sudo apt install apache2 -y
-
-# Get the instance's local IP address
-IP_ADDRESS=$(hostname -I | awk '{print $1}')
-
-# Create the HTML content with the IP address
-cat <<EOF | sudo tee /var/www/html/index.html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>BLUE</title>
-</head>
-<body style="background-color:blue; color:white;">
-    <h1>Hello from BLUE Environment</h1>
-    <p>Server IP: $IP_ADDRESS</p>
-</body>
-</html>
-EOF
-
-# Start and enable Apache
-sudo systemctl start apache2
-sudo systemctl enable apache2
-sudo systemctl restart apache2
-
+echo '<!DOCTYPE html><html><head><title>BLUE</title></head><body style="background-color:blue;color:white;"><h1>Hello from BLUE Environment</h1></body></html>' > /var/www/html/index.html
+systemctl start apache2
+systemctl enable apache2
+systemctl restart apache2
 echo "Welcome to Blue Apache Server"
