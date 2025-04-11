3a47769f44ed8ec9c9cdf640a0b1752755b9111a
minlawi <minlawi1992@gmail.com>
2025-04-12 02:19:57 +0800

    Updated providers.tf

--- providers.tf
+++ providers.tf
@@ -30,7 +30,7 @@ locals {
   server_t2_micro = "t2.micro"
   icmp_protocol   = "icmp"
   target_group_map = {
-    blue  = aws_lb_target_group.blue_tg[0].arn
-    green = aws_lb_target_group.green_tg[0].arn
+    blue  = try(aws_lb_target_group.blue_tg[0].arn, "")
+    green = try(aws_lb_target_group.green_tg[0].arn, "")
   }
 }
