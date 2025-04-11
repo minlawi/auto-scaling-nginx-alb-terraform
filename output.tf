6ba96cd99c56d6cdbba9b7777736301848d2122c
minlawi <minlawi1992@gmail.com>
2025-04-12 01:27:03 +0800

    Updated output.tf

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "alb_dns_name" {
 output "bastion_public_ip" {
   value = var.create_bastion ? aws_instance.bastion_instance[0].public_ip : null
 }
+
+output "blue_instance_private_ip" {
+  value = var.create_vpc ? data.aws_instances.blue_instance[0].private_ip : null
+}
+
+output "green_instance_private_ip" {
+  value = var.create_vpc ? data.aws_instances.green_instance[0].private_ip : null
+}
