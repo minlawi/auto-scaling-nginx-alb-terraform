64e32061d52a759da82edaf14697a1ed23a24d48
minlawi <minlawi1992@gmail.com>
2025-04-12 12:47:46 +0800

    Updated the providers.tf

--- providers.tf
+++ providers.tf
@@ -5,14 +5,14 @@ terraform {
       version = "5.89.0"
     }
   }
-  # backend "s3" {
-  #   bucket       = "lawi-bucket"
-  #   key          = "terraform.tfstate"
-  #   region       = "ap-southeast-1"
-  #   encrypt      = true
-  #   profile      = "master-programmatic-admin"
-  #   use_lockfile = true // Terrafrom version 1.10 and above locks the state file to prevent concurrent modifications
-  # }
+  backend "s3" {
+    bucket       = "lawi-bucket"
+    key          = "terraform.tfstate"
+    region       = "ap-southeast-1"
+    encrypt      = true
+    profile      = "master-programmatic-admin"
+    use_lockfile = true // Terrafrom version 1.10 and above locks the state file to prevent concurrent modifications
+  }
 }
 
 provider "aws" {
